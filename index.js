8a2b1be0142bd7d88ba2b2cfa3429816ad658e6d
Antony Santero <antony_santero@hotmail.com>
2025-11-06 09:25:52 +0100

    post main

---
+++ index.js
@@ -0,0 +1,160 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+var session = require('client-sessions');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.use(session({
+ cookieName: 'session',
+ secret: 'MonCodeSecretSession', 
+ duration: 30 * 60 * 10000,
+ activeDuration: 5 * 60 * 10000,
+ httpOnly: true,
+ secure: true,
+ ephemeral: true
+}));
+
+var connection = mysql.createConnection({
+    host     : '192.168.4.1',
+    user     : 'sqlasantero',
+    password : 'savary',
+    database : 'asantero_miniblog',
+    ssl  : {
+      rejectUnauthorized: false
+    }
+  });
+   
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("Connection ok")
+});
+
+const now = new Date();
+const formattedDate = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
+
+function requireLogin(req, res, next) {
+  if (req.session.user === undefined) {
+     res.render('login', {page: "login"});
+    } else {
+      next();
+    }
+};
+
+app.post('/login', function (req, res) {
+ var login = req.body.login.toString();
+ var mdp = req.body.mdp.toString();
+ if(login== 'monlogin' && mdp=='Passw0rd'){
+   req.session.user={login:login,mdp :mdp};
+   res.redirect('/');
+ }
+ else res.render('login') ;
+ });
+
+app.get('/logout', function (req, res) {
+    req.session.reset();
+    res.redirect('/');
+});
+
+app.get('/article/:id',(req, res) => {
+    let id=req.params.id;
+    connection.query('SELECT * FROM articles WHERE id='+id, (err, results) => {
+      if (err) return;
+      connection.query( 'SELECT * FROM comments WHERE idArticle='+id, (err, results2) => {
+        if (err) return;
+        res.render('article',{item:results[0],comments:results2});
+      });  
+    });
+});
+
+app.post('/article/:id',(req, res) => {
+    let id=req.params.id;
+
+    const sql = 'INSERT INTO comments (pseudo, comm, dateajout,idArticle) VALUES (?, ?, ?, ?)';
+    const values = [req.body.from, req.body.comm,formattedDate,id];
+    connection.query(sql, values, (err, result) => {
+      if (err) throw err;
+      console.log('Row inserted with ID:', result.insertId);
+    });
+    res.redirect(`/article/${id}`);
+   });
+
+app.get('/new',requireLogin,(req, res) => {
+    res.render('new');
+   });
+
+app.post('/new',(req, res) => {
+    const sql = 'INSERT INTO articles (title, content, image) VALUES (?, ?, ?)';
+    const values = [req.body.title, req.body.content,req.body.image];
+    connection.query(sql, values, (err, result) => {
+      if (err) throw err;
+      console.log('Row inserted with ID:', result.insertId);
+    });
+    res.redirect('/');
+   });
+
+app.get('/',(req, res) => {
+  connection.query('SELECT * FROM articles', (err, results) => {
+    if (err)return;
+    res.render('index',{articles:results});
+  });
+});
+
+
+/* api */
+app.get('/api/list/articles',(req, res) => {
+  connection.query('SELECT * FROM articles', (err, results) => {
+    if (err)return;
+    res.json(results);
+  });
+});
+
+app.get('/api/list/comments/:id',(req, res) => {
+  let id=req.params.id;
+  connection.query('SELECT * FROM comments where idArticle='+id, (err, results) => {
+    if (err)return;
+    res.json(results);
+  });
+});
+
+app.post('/api/newarticle',(req, res) => {
+
+    const sql = 'INSERT INTO articles (title, content, image) VALUES (?, ?, ?)';
+    const values = [req.body.title, req.body.content,req.body.image];
+    connection.query(sql, values, (err, result) => {
+      if (err) throw err;
+      console.log('Row inserted with ID:', result.insertId);
+    });
+    res.send(200);
+   });
+
+app.post('/api/newcomment/:id',(req, res) => {
+    let id=req.params.id;
+    const sql = 'INSERT INTO comments (pseudo, comm, dateajout,idArticle) VALUES (?, ?, ?, ?)';
+    const values = [req.body.pseudo, req.body.comm,formattedDate,id];
+    connection.query(sql, values, (err, result) => {
+      if (err) throw err;
+      console.log('Row inserted with ID:', result.insertId);
+    });
+    res.send(200);
+   });
+
+
+
+
+
+
+
+app.listen(3000, () => {
+ console.log("Serveur démarré");
+});
